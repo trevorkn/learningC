5ebae2ba5fca343f55fe2d07779742478f278368
Trevor Kipkemboi <trevorngetich03@gmail.com>
2024-12-17 17:04:45 +0300

    C code for inputing your name

---
+++ input/inputingname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//inputing name
+int main(){
+
+    char name[20];
+
+    printf("Enter your name:");
+    fgets(name, 20, stdin);
+
+    printf("Your name is %s", name);
+
+return 0;
+}
+
