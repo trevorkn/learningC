fa45a6e434fcfc7d4a3d0619ee146065dccfee71
Trevor Kipkemboi <trevorngetich03@gmail.com>
2024-12-17 17:21:17 +0300

    How to input number in C

---
+++ input/inputingnumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main(){
+
+    int myNum;
+
+    printf("Enter your number:");
+
+    scanf("%d", &myNum);
+
+    printf("My number is %d\n", myNum);
+
+return 0;
+}
