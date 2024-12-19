240e0485099b57f1d85c64ebb80b2ac06a26335f
Trevor Kipkemboi <trevorngetich03@gmail.com>
2024-12-19 03:53:48 +0300

    doing math with decimals

---
+++ input/doingmath2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main(){
+
+    float num1;
+    float num2;
+
+     printf("Enter first number with decimal:");
+     scanf("%f", &num1);
+
+     printf("Enter second number with decimal:");
+     scanf("%f", &num2);
+
+     printf("Answer:%f\n", num1 * num2);
+
+return 0;
+}
