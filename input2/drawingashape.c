65ef4b5bc9b9b44e32a1f9e33282c26fbf213fb1
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-01-20 00:14:47 +0300

    drawing a triangle

---
+++ input2/drawingashape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+  int main(){
+      printf(" __________\n");
+      printf("|         /\n");
+      printf("|        /\n");
+      printf("|       /\n");
+      printf("|      /\n");
+      printf("|     /\n");
+      printf("|    /\n");
+      printf("|   /\n");
+      printf("|  /\n");
+      printf("| /\n");
+      printf("|/\n");
+
+ return 0;
+}
