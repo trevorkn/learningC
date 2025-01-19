a2b6a31461005328f74513d5b35159097a050aa7
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-01-20 00:57:37 +0300

    using variables int and char

---
+++ output2/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int age = 40;
+    char name[] = "Dove";
+
+    printf("There was a woman called %s\n", name);
+    printf("She was %d years old\n", age);
+    printf("She liked being called %s\n", name);
+    printf("but she did not like being %d\n", age);
+
+return 0;
+}
